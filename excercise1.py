67c7eb132bcae2b5217e4eb57feeb6bd58e7956c
Vladimir I. Polyakov <johndoe1492@yandex.ru>
2017-12-08 01:52:10 +0300

    excercise 1

---
+++ excercise1.py
@@ -0,0 +1,7 @@
+#Create a program that asks the user to enter their name and their age. 
+#Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+name = raw_input(name)
+age = (raw_input(age)+100)
+
+print "Hello, %s, you'll be 100 years old at %s"
