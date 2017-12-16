91efce4b216cdc9c17e6fda1aea896892e768052
Vladimir I. Polyakov <johndoe1492@yandex.ru>
2017-12-16 23:49:03 +0300

    the rest of today excercises

---
+++ Excercise4(NOT SOLVED).py
@@ -0,0 +1,9 @@
+##Create a program that asks the user for a number and then prints out a list of all the divisors of that number. 
+
+def divisors(n):
+    n = int(raw_input("enter the number "))
+    x = range(0,n)
+   for element in range:
+
+
+       ###NOT SOLVED
