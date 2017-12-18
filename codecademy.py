b7d1a4f6f2937b1aaa185e2fe05aa28ed60cc9ec
Vladimir I. Polyakov <johndoe1492@yandex.ru>
2017-12-18 17:21:25 +0300

    codecademy excercise

---
+++ codecademy.py
@@ -0,0 +1,18 @@
+n = ["Michael", "Lieberman"]
+
+def join_strings(words):
+  result = ""
+  for n in words:
+    result.append(n)
+    return result
+
+
+print join_strings(n)
+
+
+
+
+#Define a function called join_strings accepts an argument called words. It will be a list.
+#Inside the function, create a variable called result and set it to "", an empty string.
+#Iterate through the words list and append each word to result.
+#Finally, return the result.
