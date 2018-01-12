7a7211c10f6e1e20f0a2dfb26c9e36fc7ea834f8
Vladimir I. Polyakov <johndoe1492@yandex.ru>
2018-01-12 23:41:55 +0300

    commit

--- antivowel.py
+++ antivowel.py
@@ -5,4 +5,25 @@ For example: anti_vowel("Hey You!") should return
 "Hy Y!". Don't count Y as a vowel. Make sure to remove lowercase and uppercase vowels.'''
 
 def anti_vowel(text):
-    text = raw_input
+    text = raw_input("Enter text here ")
+    vowels = ("A", "E", "I", "O", "U", "a", "e", "i", "o", "u")
+    de_voweled = ""
+    for a in text:
+        if a in vowels:
+            
+
+anti_vowel("big")
+
+
+
+
+n = ["Michael", "Lieberman"]
+
+def join_strings(words):
+  result = ""
+  for n in words:
+    result.append(n)
+    return result
+
+
+print join_strings(n)
